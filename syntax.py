836f3e7151cb11abc78884acf7d26635fcfbaaaf
DecoyChild <32802838+DecoyChild@users.noreply.github.com>
2020-09-14 14:28:08 -0400

    Add files via upload

---
+++ syntax.py
@@ -0,0 +1,13 @@
+#syntax for lists, dictionaries and 
+
+# This is a list: 
+listA = ['element 1', 'element 2', 'element 3']
+
+#tuple
+tupleEx = ('element 1', 'element 2', 'element 3')
+
+#set
+setA = {'element 1', 'element 2', 'element 3'}
+
+#Dictionary
+dictA = {'name': 'Shayne', 'age':'37'}
