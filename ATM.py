b83f5828f8284e096fb31030db35d35e28f75dc4
DecoyChild <32802838+DecoyChild@users.noreply.github.com>
2020-09-29 20:08:21 -0400

    Add files via upload

--- ATM.py
+++ ATM.py
@@ -1,53 +1,65 @@
+'''
+Shayne Greene
+ATM Script Draft 9/29/20
+Ver. A.2
+
+Script acts as an atm. Allows a customer to deposit, withdraw, and show account balance.
+'''
 import sys
 
 account_balance = float(500.25)
 
-#printbalance function
+#Print the account balance
 def printbalance():
 	print('Your current balance:')
-	print(float(account_balance))
+	print(account_balance)
 
-#deposit function
+#function to allow deposits
 def deposit(account_balance):
-	deposit_amount = input('How much would you like to deposit today?\n')
-	deposit_amount = float(deposit_amount)
 
-	#adding the deposit amount to the accountBalance
+	#taking input on the deposit amount 
+	deposit_amount = input('How much would you like to deposit today?\n')
+	#cast to a float for readability
+	deposit_amount = float(deposit_amount) 
+
+	#adding the deposit to the accountBalance
 	account_balance += float(deposit_amount)
     
-	print('Deposit was $%.2f' % deposit_amount + 
+    print('Deposit was $%.2f' % deposit_amount + 
     	', current balance is $%.2f' % account_balance)
 
-
-#withdraw function
+#withdraws given amount from account_balance
 def withdraw(account_balance):
+
 	#Get withdraw amount from user
 	withdrawal_amount = input('How much would you like to withdraw today?\n')
-	withdrawal_amount = float(withdrawal_amount) #cast into float for easy fomatting later
+	#cast input to float for readability 
+	withdrawal_amount = float(withdrawal_amount) 
 
-	#checks if withdraw is greater than balance
+	#checks if withdraw is less than account balance, otherwise nothing is updated
 	if(withdrawal_amount < account_balance):
 		account_balance -= float(withdrawal_amount)
 
-		print('Withdrawal was $%.2f' % withdrawal_amount + 
+		print('Withdrawal amount was $%.2f' % withdrawal_amount + 
 			', current balance is $%.2f' % account_balance)
 
-	#FIXMEEEE
-	# if withdraw is greater than acct balance --> $700.00 is greater than your account balance of $500.25	
 	else:
-		print('Withdrawal amount was $%.2f' % withdrawal_amount + ', current balance is $%.2f' 
+		print('$%.2f' % withdrawal_amount + ' is greater than your account balance of $%.2f' 
 			% account_balance)
 
-  
-userchoice = input ("What would you like to do?\n")
 
-if (userchoice == 'D'):
+#"menu" portion (would add a while loop to add repeating menu feature)
+#Calls functions depending on userChoice  
+userChoice = input ("What would you like to do?\n")
+
+if (userChoice == 'D'):
     deposit (account_balance)
 
-elif (userchoice == 'B'):
+elif (userChoice == 'B'):
 	printbalance()
 
-elif (userchoice == "W"):
+elif (userChoice == 'W'):
 	withdraw(account_balance)
 
-#	"{:.2f}".format(float(item_total))
+else:
+	print('Thank you for banking with us.')
