e7400c84d22aa4977bffaba54c03009bf1e3cf48
DecoyChild <32802838+DecoyChild@users.noreply.github.com>
2020-10-02 13:10:29 -0400

    Update fileRead.py

--- fileRead.py
+++ fileRead.py
@@ -13,3 +13,27 @@ var1 = open(fileName, xt)
 
 # to close (in order to release for use)
 var1.close()
+
+#matches only the beginning of the string
+# match()
+if re.match('Mad', cerseiSpeech):
+  print("Pattern matched!")
+else:
+  print("No pattern match.")
+
+# search()
+pattern = "your"
+if re.search(pattern, cerseiSpeech):
+  print("Pattern matched!")
+else:
+  print("No pattern match.")
+
+# findall()
+pattern = "your"
+results = re.findall(pattern, cerseiSpeech)
+print(results)
+
+# sub()
+results1 = re.sub ('her', 'HER', cerseiSpeech)
+results2 = re.sub ('she', 'SHE', results1)
+print(results2)
